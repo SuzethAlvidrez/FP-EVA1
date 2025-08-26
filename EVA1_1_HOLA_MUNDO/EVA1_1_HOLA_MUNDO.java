316225692d32996cfd38d2e6e74f2211aad9c11b
SuzethAlvidrez <L25550692@chihuahua2.tecnm.mx>
2025-08-26 16:19:09 -0600

    Add files via upload

---
+++ EVA1_1_HOLA_MUNDO/EVA1_1_HOLA_MUNDO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_hola_mundo;
+
+/**
+ *
+ * @author suzet
+ */
+public class EVA1_1_HOLA_MUNDO {
+
+    public static void main(String[] args) { 
+        System.out.println("HOLA MUNDO!!"); 
+        System.out.println("FUNDAMENTOS DE PROGRMACIÓN");
+    
+    
+}
+}
