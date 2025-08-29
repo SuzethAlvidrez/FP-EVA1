846bb63084a3031948450dfb1cf8fc6128c7d8e1
SuzethAlvidrez <L25550692@chihuahua2.tecnm.mx>
2025-08-28 21:06:33 -0600

    Add files via upload

---
+++ EVA1-5-VARIABLES-25550692/src/eva1/pkg5/variables/pkg25550692/EVA15VARIABLES25550692.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1.pkg5.variables.pkg25550692;
+
+/**
+ *
+ * @author suzet
+ */
+public class EVA15VARIABLES25550692 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //DECLARAR  LAS VARIABLES
+        //TIPO DE DATO     NOMBRE DEL IDENTIFICADOR
+        String Nombre;
+        int Edad;
+        double salario;
+        //INICIALIZACIÓN
+        //IDENTIFICADOR:VALOR;
+        //= ----OPERADOR  DE ASIGNACIÓN
+        Nombre= "Suzeth Alvidrez";
+        Edad= 18;
+        salario=1.1;
+        System.out.println(Nombre);
+        System.out.println(Edad);
+        System.out.println(salario);
+        
+        
+    }
+    
+}
